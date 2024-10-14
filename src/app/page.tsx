7fcfe8690f4cec0271f9b38cdf0649c094fce23a
Yash <er.yshsingh@gmail.com>
2024-10-15 02:00:02 +0530

    added Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-    <h1 className="text-3xl text-center text-green-600">Start</h1>
+    
+
+    <HeroSection>
+
+    </HeroSection>
+
     </main>
   );
 }
